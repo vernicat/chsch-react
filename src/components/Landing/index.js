35796105607be8ca71d7f114d6162631a86467b1
vernicat <catherine.vernigorova@gmail.com>
2021-05-11 01:06:17 +0300

    add pug

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -4,14 +4,7 @@ import './Landing.styl';
 
 const Landing = () => {
     return(
-      <div>
-        <a class="main-item" href="javascript:void(0);" tabindex="1" >Открыть подменю</a> 
-        <ul class="sub-menu"> 
-          <li><a href="#1">подпункт 1</a></li> 
-          <li><a href="#2">подпункт 2</a></li> 
-          <li><a href="#3">подпункт 3</a></li> 
-        </ul> 
-      </div>
+      pug`p TEST`
     );
 }
 
